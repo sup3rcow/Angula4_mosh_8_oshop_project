8d9e01c4d31e362b9ee70517c769b3948f9478bf
sup3rcow <bullandrooster@gmail.com>
2017-11-14 23:00:37 +0100

    bs-navbar

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BsNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
