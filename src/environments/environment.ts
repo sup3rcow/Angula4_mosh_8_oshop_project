e25563babdc95de29757988ea372f3ae3bde2fa5
sup3rcow <bullandrooster@gmail.com>
2017-11-15 16:50:38 +0100

    firebase, navbar

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBHhRdRP7yOAEXGwrscKlxpXgCAylIuc_Q',
-    authDomain: 'oshop-729a4.firebaseapp.com',
-    databaseURL: 'https://oshop-729a4.firebaseio.com',
-    projectId: 'oshop-729a4',
-    storageBucket: 'oshop-729a4.appspot.com',
-    messagingSenderId: '167585925374'
+    apiKey: 'AIzaSyDjkMvaExntBa2UDauFhtyZNp0k15vGQFU',
+    authDomain: 'oshop-b60d8.firebaseapp.com',
+    databaseURL: 'https://oshop-b60d8.firebaseio.com',
+    projectId: 'oshop-b60d8',
+    storageBucket: '',
+    messagingSenderId: '607376833920'
   }
 };
